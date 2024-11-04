67c358f0ade98c6b56aef165d6fee0cd72fabbc0
Preeti Vadali <preeti.abhiram@gmail.com>
2024-11-05 00:05:41 +0100

    created a function to display the total amount of the account

--- index.js
+++ index.js
@@ -1,4 +1,8 @@
 let account ={
     accountName : "Preeti",
     balance : 100000,
+    getBalance() {
+console.log(`the total amount in your account is ${this.balance.toFixed(2)} kronas` )
+    }
 }
+account.getBalance();
