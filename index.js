7d7ac108f1bff6f0818edf6fec87e53b910db535
Preeti Vadali <preeti.abhiram@gmail.com>
2024-11-06 23:35:20 +0100

    created a function which deposited money in the user account

--- index.js
+++ index.js
@@ -1,8 +1,24 @@
-let account ={
-    accountName : "Preeti",
-    balance : 100000,
+let account = {
+    accountName: "Preeti",
+    balance: 100000,
+    
     getBalance() {
-console.log(`the total amount in your account is ${this.balance.toFixed(2)} kronas` )
-    }
-}
+        console.log(`The total amount in your account is ${this.balance.toFixed(2)} kronas`);
+    },
+
+    deposit(amount) {
+        if (typeof amount === "number" && amount > 0) {
+            this.balance += amount;
+            console.log(`${amount.toFixed(2)} is deposited in your account.`);
+        } else {
+            console.log("Nothing deposited");
+        }
+    },
+
+   
+};
+
+// Testing the methods
 account.getBalance();
+account.deposit(500); 
+
