79a11c532594dd012d56354185cfc9fe4633d0fb
Preeti Vadali <preeti.abhiram@gmail.com>
2024-11-10 00:48:16 +0100

    created a function which shows error message

--- index.js
+++ index.js
@@ -4,6 +4,7 @@ let account = {
     
     getBalance() {
         console.log(`The total amount in your account is ${this.balance.toFixed(2)} kronas`);
+        
     },
 
     deposit(amount) {
@@ -20,14 +21,30 @@ let account = {
             this.balance -= drawn;
             console.log(`${drawn.toFixed(2)} is withdrawn from your account.`);
         } else {
-            console.log("Nothing is winthdrawn");
+           console.log("Nothing is withdrawn");
         }
-    }
-   
+    },
+    getAccountName(){
+        console.log(`${this.accountName} is the account holders name `);
+    },
+
+   accountError(account){
+     
+        if (typeof account.balance != "number" || account.balance < 0) {
+           
+            console.log(`something wrong. Error 101`);
+        } else{
+            console.log(`Everything is correct`)
+        }
+   }
+  
+    
 };
 
-// Testing the methods
+
 account.getBalance();
 account.deposit(500); 
 account.withdrawal(100);
+account.getAccountName()
+account.accountError( account)
 
